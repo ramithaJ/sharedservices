a8ff1bfda6acaa022e034a440a71695a769f4f6c
Kalyan <kkopella@wiley.com>
2015-06-09 14:34:45 +0530

    0000 | kkopella | Added Shared Service Exception class

---
+++ as2userservices/src/main/java/com/wiley/gr/ace/sharedservices/exceptions/SharedServiceException.java
@@ -0,0 +1,11 @@
+package com.wiley.gr.ace.sharedservices.exceptions;
+
+/**
+ * Created by kkalyan on 6/9/2015.
+ */
+public class SharedServiceException extends Exception {
+
+    public SharedServiceException(String message) {
+        super(message);
+    }
+}
