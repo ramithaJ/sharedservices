0ca5a05bf6ecb445ad0b53e59f7f0aa73325ca9b
aqahmed <aqahmed@wiley.com>
2015-06-16 14:06:16 +0530

    AS2-33 | aqahmed | notification template

---
+++ as2notificationandtemplate/model/src/main/java/com/wiley/gr/ace/authorservices/model/Tags.java
@@ -0,0 +1,27 @@
+package com.wiley.gr.ace.authorservices.model;
+
+import java.util.List;
+
+public class Tags {
+	private List<String> tag1List;
+	
+	private List<String> tag2List;
+	
+	public List<String> getTag1List() {
+
+		return tag1List;
+	}
+
+	public void setTag1List(List<String> tag1List) {
+		this.tag1List = tag1List;
+	}
+	
+	public List<String> getTag2List() {
+		return tag2List;
+	}
+
+	public void setTag2List(List<String> tag2List) {
+		this.tag2List = tag2List;
+	}
+	
+}
