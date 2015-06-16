9bfe263cf9e8bfa688e6fd2678f7f21d56a16d7f
aqahmed <aqahmed@wiley.com>
2015-06-16 14:02:25 +0530

    AS2-33 | aqahmed | notification template

--- as2notificationandtemplate/web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/TemplateManagementController.java
+++ as2notificationandtemplate/web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/TemplateManagementController.java
@@ -3,6 +3,8 @@
  */
 package com.wiley.gr.ace.authorservices.web.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.wiley.gr.ace.authorservices.model.ErrorPOJO;
 import com.wiley.gr.ace.authorservices.model.Service;
+import com.wiley.gr.ace.authorservices.model.Tags;
 import com.wiley.gr.ace.authorservices.model.TemplateDetails;
 import com.wiley.gr.ace.authorservices.model.TemplateVO;
 import com.wiley.gr.ace.authorservices.services.service.TemplateManagementService;
@@ -31,9 +34,23 @@ public class TemplateManagementController {
 	@Autowired(required = true)
 	TemplateManagementService templateManagementService;
 
-	@RequestMapping(value = "/$applicationId/tags", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Service getTemplateTags() {
+	@RequestMapping(value = "/{applicationId}/tags", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Service getTemplateTags(@PathVariable String applicationId) {
 		Service service = new Service();
+		Tags tags = null;
+		try {
+			tags = templateManagementService.getTemplateTags(applicationId);
+			if (!StringUtils.isEmpty(tags)) {
+				service.setStatus("SUCCESS");
+				service.setPayload(tags);
+			}
+		} catch (Exception e) {
+			ErrorPOJO error = new ErrorPOJO();
+			error.setCode(205);
+			error.setMessage("Error Fetching Template");
+			service.setStatus("ERROR");
+			service.setError(error);
+		}
 		return service;
 	}
 
@@ -60,15 +77,57 @@ public class TemplateManagementController {
 		return service;
 	}
 
-	@RequestMapping(value = "applicationId/$templateId", method = RequestMethod.PUT)
-	public @ResponseBody Service insertTemplateId() {
+	@RequestMapping(value = "/createTemplate", method = RequestMethod.PUT)
+	public @ResponseBody Service insertTemplate(@RequestBody TemplateVO template) {
 		Service service = new Service();
+		boolean isCreated=false;
+		try {
+			isCreated = templateManagementService.insertTemplate(template);
+		} catch (Exception e) {
+			ErrorPOJO error = new ErrorPOJO();
+			error.setCode(205);
+			error.setMessage("Error Fetching Template");
+			service.setStatus("ERROR");
+			service.setError(error);
+		}
+		if (isCreated) {
+			service.setStatus("SUCCESS");
+			service.setPayload(isCreated);
+		}
+
+		else {
+			service.setStatus("FAILURE");
+			service.setPayload(isCreated);
+
+		}
 		return service;
 	}
 
-	@RequestMapping(value = "/$applicationId/$templateId", method = RequestMethod.POST)
-	public @ResponseBody Service updateTemplateId() {
+	@RequestMapping(value = "/{applicationId}/{templateId}", method = RequestMethod.POST)
+	public @ResponseBody Service updateTemplate(@PathVariable("templateId") String templateId,
+			@PathVariable("applicationId") String applicationId,
+			@RequestBody Map<String,Object> templateMap) {
 		Service service = new Service();
+		boolean isUpdated = false;
+				try{
+					isUpdated=templateManagementService.updateTemplate(templateId, applicationId, templateMap);
+				}catch (Exception e) {
+					ErrorPOJO error = new ErrorPOJO();
+					error.setCode(205);
+					error.setMessage("Error Fetching Template");
+					service.setStatus("ERROR");
+					service.setError(error);
+				}
+				if (isUpdated) {
+					service.setStatus("SUCCESS");
+					service.setPayload(isUpdated);
+				}
+
+				else {
+					service.setStatus("FAILURE");
+					service.setPayload(isUpdated);
+
+				}
 		return service;
 	}
 
@@ -77,8 +136,17 @@ public class TemplateManagementController {
 			@PathVariable("templateId") String templateId,
 			@PathVariable("applicationId") String applicationId) {
 		Service service = new Service();
-		boolean isDeleted = templateManagementService.deleteTemplate(
+		boolean isDeleted=false;
+		try{
+		isDeleted = templateManagementService.deleteTemplate(
 				templateId, applicationId);
+		}catch (Exception e) {
+			ErrorPOJO error = new ErrorPOJO();
+			error.setCode(205);
+			error.setMessage("Error Fetching Template");
+			service.setStatus("ERROR");
+			service.setError(error);
+		}
 		if (isDeleted) {
 			service.setStatus("SUCCESS");
 			service.setPayload(isDeleted);
@@ -116,7 +184,7 @@ public class TemplateManagementController {
 		return service;
 	}
 
-	@RequestMapping(value = "/{applicationId}/{templateId}/render", method = RequestMethod.POST)
+	@RequestMapping(value = "/{applicationId}/{templateId}/render", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Service renderTemplate(
 			@PathVariable("templateId") String templateId,
 			@PathVariable("applicationId") String applicationId,
