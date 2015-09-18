aaeb2a8b583f6088b480d5e86f4170817c8250e2
Shiva <skpunna@wiley.com>
2015-09-18 16:35:22 +0530

    As2-35 | skpunna | user login management

---
+++ as2authenticationservices/src/main/java/com/wiley/gr/ace/auth/security/model/VerifyUserResponse.java
@@ -0,0 +1,31 @@
+package com.wiley.gr.ace.auth.security.model;
+
+/**
+ * @author virtusa version 1.0
+ *
+ */
+public class VerifyUserResponse {
+
+	/**
+	 * the value of status.
+	 */
+	private  String status;
+
+	/**
+	 * @return the status
+	 */
+	public final String getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public final void setStatus(String status) {
+		this.status = status;
+	}
+
+	
+	
+	
+}
