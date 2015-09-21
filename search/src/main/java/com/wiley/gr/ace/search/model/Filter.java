0d8ef9791ff4a20a234cb6e0536ffbfcf6d92959
rajat <rgoel@wiley.com>
2015-09-21 12:56:36 +0530

    AS-178 | rgoel | created SimpleQuery and Filter

---
+++ search/src/main/java/com/wiley/gr/ace/search/model/Filter.java
@@ -0,0 +1,15 @@
+package com.wiley.gr.ace.search.model;
+
+import java.util.Map;
+
+public class Filter {
+	private Map<String, String> term;
+
+	public Map<String, String> getTerm() {
+		return term;
+	}
+
+	public void setTerm(Map<String, String> term) {
+		this.term = term;
+	}
+}
---
+++ search/src/main/java/com/wiley/gr/ace/search/model/SimpleQuery.java
@@ -0,0 +1,36 @@
+package com.wiley.gr.ace.search.model;
+
+
+public class SimpleQuery {
+	private String field;
+	private String value;
+	private String from;
+	private String to;
+	
+	public String getField() {
+		return field;
+	}
+	public void setField(String field) {
+		this.field = field;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	
+	
+}
